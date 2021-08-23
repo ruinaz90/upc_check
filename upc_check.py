a6db98f7f5cda4bf9f94a9f67eeb45807ff0783e
rzhao28 <arenzi28@outlook.com>
2021-08-23 07:05:11 -0400

    Added UPC calculations

--- upc_check.py
+++ upc_check.py
@@ -1,6 +1,22 @@
 import pyinputplus as pyip
 
-print("Enter UPC: ")
-upc_number = pyip.inputNum()
+upc_input = pyip.inputNum("Enter UPC: ")
+upc_number = [int(num) for num in str(upc_input)]   # Convert int input to list
 
-print(f"UPC number is {upc_number}")
+upc_odd = upc_number[:10:2]   # digits in odd position
+upc_even = upc_number[1:9:2] # digits in even position
+sum_odd = 0
+sum_even = 0
+
+# Add digits in odd position and multiply result by 3
+for digit in upc_odd:
+    sum_odd += digit
+sum_odd *= 3
+
+# Add digits in even position
+for digit in upc_even:
+    sum_even += digit
+
+sum = sum_odd + sum_even
+
+print(upc_number[10])
